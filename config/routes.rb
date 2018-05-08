146317310f21a5381aa03944616f37005b196dc5
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-08 09:04:45 -0600

    Logout path

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
 
   get '/auth/spotify', as: :spotify_login
   get '/auth/spotify/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
 end
