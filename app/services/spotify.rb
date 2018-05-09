1b5cdf00df0a1402131a41402b899d3ca1810974
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-09 10:05:24 -0600

    Updated spotify class to be an instance

--- app/services/spotify.rb
+++ app/services/spotify.rb
@@ -1,14 +1,29 @@
 class Spotify
-  def self.check_token(user)
+  API_URL = "https://api.spotify.com/v1/"
+  
+  def initialize(user)
+    @user = user
+  end
+
+  def self.from_user(user)
+    new(user)
+  end
+
+  def check_token
     now = DateTime.now
-    user.token_expires > now
+    @user.token_expires > now
   end
 
-  def self.refresh_token(user)
-    return if check_token(user)
+  def refresh_token
+    return if check_token
 
   end
 
-  def self.make_request(endpoint)
+  def make_request(endpoint, parameters)
+    conn = Faraday::new(url: "#{API_URL}#{endpoint}?#{parameters.to_query}")
+
+    response = conn.get
+
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
--- spec/services/spotify_spec.rb
+++ spec/services/spotify_spec.rb
@@ -1,50 +1,56 @@
 require 'rails_helper'
 
 describe 'Spotify Wrapper' do
+  before(:each) do
+    DatabaseCleaner.clean
+    @user = mock_user
+    @user.save!
+    @spoti_user = Spotify::from_user(@user)
+  end
+
   describe 'token validation' do
     it 'should know that user does not need refresh' do
-      user = mock_user
-      user.token = 'abc'
-
-      result = Spotify::check_token(user)
+      result = @spoti_user.check_token
 
       expect(result).to eq(true)
     end
 
     it 'should know that user needs refresh' do
-      user = mock_user
-      user.token_expires = DateTime.now - 1.hour
+      @user.token_expires = DateTime.now - 1.hour
+      @user.save!
 
-      result = Spotify::check_token(user)
+      result = @spoti_user.check_token
 
       expect(result).to eq(false)
     end
 
     it 'should skip refresh when not needed' do
-      user = mock_user
-      user.token_expires = DateTime.now + 1.hour
-      user.save!
+      @user.token_expires = DateTime.now + 1.hour
+      @user.save!
 
-      Spotify::refresh_token(user)
+      @spoti_user = Spotify::from_user(@user)
+      @spoti_user.refresh_token
 
-      expect(User.first.token).to eq(user.token)
+      expect(User.first.token).to eq(@user.token)
     end
 
     xit 'should refresh when needed' do
-      user = mock_user
-      user.token_expires = DateTime.now - 1.hour
-      user.save!
+      @user = mock_user
+      @user.token_expires = DateTime.now - 1.hour
+      @user.save!
 
-      Spotify::refresh_token(user)
+      @spoti_user = Spotify::from_user(user)
+      @spoti_user.refresh_token
 
-      expect(User.first.token).to not_eq(user.token)
+      expect(User.first.token).to_not eq(@user.token)
     end
   end
 
   describe 'API calls' do
     it 'should be able to make an API call' do
-      result = Spotify::make_request('search')
+      result = @spoti_user.make_request('search', {q: 'charly bliss', type: 'track'})
 
+      expect(result).to be_instance_of(Hash)
       expect(result[:tracks]).to_not be_nil
       expect(result[:tracks][:items]).to be_instance_of(Array)
     end
