62dea02a239cd154ec710faf4309500152cc2929
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-10 09:00:55 -0600

    User has a collection of playlists

---
+++ app/models/playlist_collection.rb
@@ -0,0 +1,9 @@
+class PlaylistCollection
+  def self.from_spotify(spotify)
+    new(spotify)
+  end
+
+  private
+  def initialize(spotify)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   validates_presence_of :refresh_token
   validates_presence_of :token_expires
 
+  attr_reader :playlists
+
+  after_initialize do
+    spotify = Spotify::from_user(self)
+    @playlists = PlaylistCollection::from_spotify(spotify)
+  end
+
   def self.from_omniauth(auth_info)
     where(uid: auth_info[:uid]).first_or_create do |new_user| 
       new_user.uid = auth_info.uid
