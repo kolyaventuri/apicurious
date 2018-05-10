94b159b2e3e13e59394fc094c01771e072390cd7
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-10 11:26:46 -0600

    Playlists are properly displayed on page

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -4,7 +4,7 @@ class Playlist
   def initialize(playlist_data)
     @id = playlist_data[:id]
     @uri = playlist_data[:uri]
-    @name = playlist_data[:name]
+    @name = playlist_data[:name].gsub(/[^\s!-~]/i,'').strip
     @tracks_uri = playlist_data[:tracks][:href]
     @track_count = playlist_data[:tracks][:total]
     @owner = SpotifyUser.new(playlist_data[:owner])
