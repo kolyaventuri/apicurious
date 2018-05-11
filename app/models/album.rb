db6680dc5d22259919cec5dadc2287e714baf7bf
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-10 20:37:20 -0600

    Album model

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,14 @@
 class Album
+  attr_reader :id,
+    :name,
+    :artwork,
+    :artists
+
+  def initialize(album_data)
+    @id = album_data[:id]
+    @name = album_data[:name]
+    @artwork = album_data[:images].first[:url]
+
+    @artists = album_data[:artists].map { |artist| Artist.new(artist) }
+  end
 end
