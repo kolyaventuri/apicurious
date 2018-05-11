c868b83b62c6d4c5998f66090e73f43e2451aa62
Kolya Venturi <kolya.venturi@enx3s.com>
2018-05-10 21:55:46 -0600

    User can see tracks on their playlists

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   get '/auth/spotify', as: :spotify_login
   get '/auth/spotify/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy', as: 'logout'
+
+  namespace :playlists do
+    get '/:id', to: 'playlist#show'
+  end
 end
